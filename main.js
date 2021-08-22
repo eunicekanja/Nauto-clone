64ae209031df2b797ffa2b1c91d651ce8704026e
Eunice Kanja <79441182+eunicekanja@users.noreply.github.com>
2021-08-22 22:37:44 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+'use strict'
+const form=document.querySelector('.form');
+const btn=document.querySelector('.close')
+
+btn.addEventListener('click', function(){
+    form.classList.add('hidden')
+})
+
+//function close(){
+    //form//.classList.add('hidden')
+//}
